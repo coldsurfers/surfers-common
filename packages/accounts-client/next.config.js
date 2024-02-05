dc81f35f7740ff3724c15562247ee5ee5fa71fa2
yungblud <killi8n@gmail.com>
2024-02-05 18:26:05 +0900

    feat: implemented fstvllife-design-system

---
+++ packages/accounts-client/next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+
+const path = require('path')
+
+const nextConfig = {
+    experimental: {
+        externalDir: true,
+      },
+      webpack: (config) => {
+        config.resolve.alias = {
+          ...(config.resolve.alias || {}),
+          // Transform all direct `react-native` imports to `react-native-web`
+          'react-native$': 'react-native-web',
+          'styled-components': path.resolve(__dirname, './node_modules/styled-components'),
+        }
+        config.resolve.extensions = [
+          '.web.js',
+          '.web.jsx',
+          '.web.ts',
+          '.web.tsx',
+          ...config.resolve.extensions,
+        ]
+
+        return config
+      },
+      transpilePackages: ['@coldsurfers/hotsurf'],
+      compiler: {
+        styledComponents: true,
+      },
+};
+
+module.exports = nextConfig;
