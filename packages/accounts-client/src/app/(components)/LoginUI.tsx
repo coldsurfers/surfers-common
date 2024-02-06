397c0b32e28f086426a94defbf83ae2665b2dad6
yungblud <killi8n@gmail.com>
2024-02-06 13:21:01 +0900

    feat: implemented home search params

---
+++ packages/accounts-client/src/app/(components)/LoginUI.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { LoginForm } from '@coldsurfers/hotsurf'
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+  position: absolute;
+  top: 50%; /* position the top  edge of the element at the middle of the parent */
+  left: 50%; /* position the left edge of the element at the middle of the parent */
+
+  transform: translate(-50%, -50%);
+
+  padding: 1rem;
+  border-radius: 3px;
+
+  display: flex;
+  flex-direction: column;
+
+  box-shadow:
+    0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+`
+
+export function LoginUI({ afterHttpAddress }: { afterHttpAddress: string }) {
+  return (
+    <Wrapper>
+      <LoginForm formTitle="ColdSurf Accounts" onPressLoginButton={() => {}} />
+    </Wrapper>
+  )
+}
--- packages/accounts-client/src/app/page.tsx
+++ packages/accounts-client/src/app/page.tsx
@@ -1,31 +1,24 @@
-'use client'
+/* eslint-disable no-unused-vars */
+import { headers } from 'next/headers'
+import { NextPage } from 'next/types'
+import { LoginUI } from './(components)/LoginUI'
 
-import { LoginForm } from '@coldsurfers/hotsurf'
-import styled from 'styled-components'
+const Home: NextPage<{
+  searchParams: {
+    after?: string
+    clientId?: string
+  }
+}> = ({ searchParams }) => {
+  const headersList = headers()
+  const referer = headersList.get('referer')
 
-const Wrapper = styled.section`
-  position: absolute;
-  top: 50%; /* position the top  edge of the element at the middle of the parent */
-  left: 50%; /* position the left edge of the element at the middle of the parent */
+  const { after, clientId } = searchParams
 
-  transform: translate(-50%, -50%);
+  if (!after) {
+    throw Error('!!!')
+  }
 
-  padding: 1rem;
-  border-radius: 3px;
-
-  display: flex;
-  flex-direction: column;
-
-  box-shadow:
-    0 1px 3px rgba(0, 0, 0, 0.12),
-    0 1px 2px rgba(0, 0, 0, 0.24);
-  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
-`
-
-export default function Home() {
-  return (
-    <Wrapper>
-      <LoginForm formTitle="ColdSurf Accounts" onPressLoginButton={() => {}} />
-    </Wrapper>
-  )
+  return <LoginUI afterHttpAddress={after} />
 }
+
+export default Home
