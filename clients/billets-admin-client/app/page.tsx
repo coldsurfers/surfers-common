82d93f4ff53a2dcb4b4d3ca609590a631be41cbe
yungblud <killi8n@gmail.com>
2024-02-15 09:39:32 +0900

    feat: fixed build error on clients

--- clients/billets-admin-client/app/page.tsx
+++ clients/billets-admin-client/app/page.tsx
@@ -26,8 +26,8 @@ const columnHelper = createColumnHelper<Concert>()
 export default function Page() {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const pageParam = searchParams.get('page')
-  const limitParam = searchParams.get('limit')
+  const pageParam = searchParams?.get('page')
+  const limitParam = searchParams?.get('limit')
   const [page, setPage] = useState<number>(
     pageParam ? +pageParam : DEFAULT_PAGE
   )
@@ -274,9 +274,7 @@ const Table = styled.table`
   margin-top: 60px;
   background-color: ${palette.white};
   width: 90vw;
-  box-shadow:
-    0 1px 3px rgba(0, 0, 0, 0.12),
-    0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   border-radius: 3px;
 `
--- clients/billets-admin-client/ui/UploadForm.tsx
+++ clients/billets-admin-client/ui/UploadForm.tsx
@@ -110,7 +110,7 @@ const AddButton = memo(({ onPress }: { onPress: () => void }) => (
 
 const UploadForm = () => {
   const searchParams = useSearchParams()
-  const idParam = searchParams.get('id')
+  const idParam = searchParams?.get('id')
   const router = useRouter()
   const { data: concertCategoryListData, loading: concertCategoryListLoading } =
     useConcertCategoryListQuery()
@@ -740,9 +740,7 @@ const Wrapper = styled.div`
   width: 900px;
 
   background-color: ${palette.white};
-  box-shadow:
-    0 1px 3px rgba(0, 0, 0, 0.12),
-    0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 `
 
@@ -772,9 +770,7 @@ const PosterThumbnail = styled.img`
   border-radius: 8px;
   margin-top: 10px;
   margin-bottom: 10px;
-  box-shadow:
-    0 1px 3px rgba(0, 0, 0, 0.12),
-    0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   object-fit: contain;
 `
