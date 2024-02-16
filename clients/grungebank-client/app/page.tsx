7955fa9df1344a931010123101fa1f77077065de
yungblud <killi8n@gmail.com>
2024-02-16 23:33:47 +0900

    chore: added feed category badge

--- clients/grungebank-client/app/page.tsx
+++ clients/grungebank-client/app/page.tsx
@@ -1,4 +1,5 @@
 import { HomeFeedList } from '../components/Feed'
+import { FeedCategoryBadge } from '../components/FeedCategory'
 import { HomeTopInput } from '../components/Input/HomeTopInput'
 import styles from './styles.module.css'
 
@@ -6,6 +7,10 @@ export default async function Home() {
   return (
     <section className={styles.wrapper}>
       <HomeTopInput />
+      <div className="flex">
+        <FeedCategoryBadge>구인구직</FeedCategoryBadge>
+        <FeedCategoryBadge>악기장터</FeedCategoryBadge>
+      </div>
       <HomeFeedList />
     </section>
   )
