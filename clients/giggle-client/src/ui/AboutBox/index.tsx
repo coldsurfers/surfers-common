dc528bb404b4e46f3b1189e33255b750eb8f9e78
yungblud <killi8n@gmail.com>
2024-03-16 12:05:54 +0900

    feat(giggle-client): modified simple styles

--- clients/giggle-client/src/ui/AboutBox/index.tsx
+++ clients/giggle-client/src/ui/AboutBox/index.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react'
 import styles from './aboutBox.module.css'
+import Link from 'next/link'
 
 const AboutBox = () => {
   return (
@@ -8,6 +9,13 @@ const AboutBox = () => {
         <div className={styles.aboutBoxText}>Hello! Welcome to Giggle.</div>
         <div className={styles.aboutBoxText}>
           Giggle is another gig platform based on artists and venues.
+        </div>
+        <div className={styles.aboutBoxText}>
+          Giggle is in making progress with{' '}
+          <Link href={'https://coldsurf.io'}>
+            <span className={styles.aboutLink}>coldsurfers</span>
+          </Link>
+          !
         </div>
       </div>
     </div>
