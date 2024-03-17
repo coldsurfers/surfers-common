91fdeb149745635b134bf1e08c54c010650d621a
yungblud <killi8n@gmail.com>
2024-03-17 11:58:30 +0900

    feat: added input

--- clients/giggle-client/src/app/login/page.tsx
+++ clients/giggle-client/src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { useCallback } from 'react'
 import { signIn } from 'next-auth/react'
 import styled from 'styled-components'
 import LoginButton from '@/ui/Button/LoginButton'
+import Link from 'next/link'
 
 const TITLE_MESSAGE = 'Log in to Giggle'
 const LOGIN_PRE_MESSAGE = 'Continue with'
@@ -20,18 +21,27 @@ const TopTitle = styled.h1`
   margin-bottom: 1rem;
 `
 
-const GoogleLoginButton = styled(LoginButton)`
-  width: 100%;
-`
-
 const Divider = styled.div`
   height: 1px;
-  color: gray;
+  background: black;
+  width: 100%;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 `
 
 const EmailLoginForm = styled.form``
-const EmailLoginSubmitButton = styled.button``
-const EmailLoginForgotPasswordMessage = styled.p``
+const EmailLoginForgotPasswordMessage = styled.p`
+  text-decoration: underline;
+`
+
+const TextInput = styled.input`
+  padding: 1rem;
+  border-radius: 3px;
+  border: 1px solid black;
+  width: 100%;
+  font-size: 0.85rem;
+  font-weight: 600;
+`
 
 export default function LoginPage() {
   const onClickGoogleLoginButton = useCallback(() => signIn('google'), [])
@@ -45,14 +55,20 @@ export default function LoginPage() {
       >{`${LOGIN_PRE_MESSAGE} Google`}</LoginButton>
       <Divider />
       <EmailLoginForm>
-        <LoginButton
-          withScale
-          fullWidth
+        <TextInput placeholder="Email" />
+        <TextInput
+          placeholder="Password"
           style={{ marginTop: '1rem', marginBottom: '1rem' }}
-        >
+        />
+        <LoginButton withScale fullWidth style={{ marginBottom: '1rem' }}>
           {EMAIL_LOGIN_MESSAGE}
         </LoginButton>
       </EmailLoginForm>
+      <Link href="/password-reset">
+        <EmailLoginForgotPasswordMessage>
+          Forgot your password?
+        </EmailLoginForgotPasswordMessage>
+      </Link>
     </Wrapper>
   )
 }
