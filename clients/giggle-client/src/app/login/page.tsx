96779674fc7697294c38f324ad56603bfc3a5530
yungblud <killi8n@gmail.com>
2024-03-17 10:48:03 +0900

    chore: added login message

--- clients/giggle-client/src/app/login/page.tsx
+++ clients/giggle-client/src/app/login/page.tsx
@@ -3,10 +3,14 @@
 import { LoginButton } from '@/ui/Header'
 import { signIn } from 'next-auth/react'
 
+const LOGIN_PRE_MESSAGE = 'Continue with'
+
 export default function LoginPage() {
   return (
     <>
-      <LoginButton onClick={() => signIn('google')}>Google Login</LoginButton>
+      <LoginButton
+        onClick={() => signIn('google')}
+      >{`${LOGIN_PRE_MESSAGE} Google`}</LoginButton>
     </>
   )
 }
