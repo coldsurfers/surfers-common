2f7ffefdb619e2e3e14c84e84af31d026348f3bf
yungblud <killi8n@gmail.com>
2024-03-18 21:34:53 +0900

    feat: added database libs

---
+++ clients/giggle-client/src/libs/database.ts
@@ -0,0 +1,10 @@
+// for encode password for prisma postresql DATABASE_URL
+export function percentEncodeAllChars(plainPassword: string) {
+  var strEncoded = ''
+  for (var i = 0, ilen = plainPassword.length; i < ilen; i++) {
+    // @ts-ignore
+    var strHex = parseInt(plainPassword.charCodeAt(i)).toString(16)
+    strEncoded += '%' + strHex
+  }
+  return strEncoded
+}
