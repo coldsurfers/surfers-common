6144b2d7867a295f1dbaf588bba22ac0fcc884fa
yungblud <killi8n@gmail.com>
2024-03-30 16:02:23 +0900

    refactor: social veridfy

--- clients/giggle-client/src/database/services/auth/social.ts
+++ clients/giggle-client/src/database/services/auth/social.ts
@@ -1,15 +1,35 @@
 import googleOAuthClient from '@/database/libs/googleOAuthClient'
+import { LoginTicket } from 'google-auth-library'
 
-const verifyGoogleAccessToken = async (accessToken: string) => {
+export type VERIFY_GOOGLE_ACCESS_TOKEN_ERROR = 'INVALID_ACCESS_TOKEN'
+
+const verifyGoogleAccessToken = async (
+  accessToken: string
+): Promise<
+  | {
+      isError: true
+      error: VERIFY_GOOGLE_ACCESS_TOKEN_ERROR
+    }
+  | {
+      isError: false
+      data: LoginTicket
+    }
+> => {
   try {
     const verified = await googleOAuthClient.verifyIdToken({
       idToken: accessToken,
       audience: process.env.GOOGLE_OAUTH_CLIENT_ID,
     })
-    return verified
+    return {
+      isError: false,
+      data: verified,
+    }
   } catch (e) {
     console.error(e)
-    return undefined
+    return {
+      isError: true,
+      error: 'INVALID_ACCESS_TOKEN',
+    }
   }
 }
 
