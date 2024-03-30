35c2ab3a418568a28f58e2e60deda76e2a624cf3
yungblud <killi8n@gmail.com>
2024-03-30 13:17:45 +0900

    feat: setup credentials email signup

--- clients/giggle-client/src/app/api/auth/signup/route.ts
+++ clients/giggle-client/src/app/api/auth/signup/route.ts
@@ -1,6 +1,33 @@
-export const POST = async (request: Request) => {
-  const requestBody = await request.json()
-  console.log(requestBody)
+import AuthSignUpService from '@/database/services/auth/signUp'
 
-  return Response.json({ hello: 'world!' })
+export const POST = async (request: Request) => {
+  try {
+    const requestBody = (await request.json()) as
+      | {
+          provider: 'credentials'
+          email: string
+          password: string
+          passwordConfirm: string
+        }
+      | {
+          provider: 'google'
+          email: string
+        }
+
+    if (requestBody.provider === 'credentials') {
+      const result = await AuthSignUpService.emailSignUp({
+        email: requestBody.email,
+        password: requestBody.password,
+        passwordConfirm: requestBody.passwordConfirm,
+      })
+      if (result.isError) {
+        return Response.json(result)
+      }
+      return result.data
+    }
+
+    return Response.json({ isError: true, error: 'INVALID_PROVIDER' })
+  } catch (e) {
+    return Response.json({ isError: true, error: 'UNKNOWN_ERROR' })
+  }
 }
