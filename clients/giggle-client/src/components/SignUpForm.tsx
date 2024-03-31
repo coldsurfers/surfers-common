20276fe9598f7d04da10e6d7eba3255533dd6a9e
yungblud <killi8n@gmail.com>
2024-03-31 12:48:06 +0900

    feat: implemented signup form with store

--- clients/giggle-client/src/components/SignUpForm.tsx
+++ clients/giggle-client/src/components/SignUpForm.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import log from '@/libs/log'
 import LoginButton from '@/ui/Button/LoginButton'
 import { signIn } from 'next-auth/react'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import styled from 'styled-components'
+import { useRouter } from 'next/navigation'
+import { useSignUpStore } from '@/stores/SignUpStore'
+import { z } from 'zod'
 
 const TITLE_MESSAGE = `Sign up to start finding venues`
 const EMAIL_NEXT_MESSAGE = 'Next'
@@ -45,24 +47,59 @@ const EmailNextButton = styled(LoginButton)`
   margin-top: 1rem;
 `
 
+const InputsEmailSchema = z.string().email()
+
 type Inputs = {
-  email: string
+  email: z.TypeOf<typeof InputsEmailSchema>
 }
 
 export default function SignUpForm() {
+  const router = useRouter()
+  const { increaseStep, decreaseStep, setErrorMessage } = useSignUpStore(
+    (state) => ({
+      increaseStep: state.increaseStep,
+      decreaseStep: state.decreaseStep,
+      setErrorMessage: state.setErrorMessage,
+    })
+  )
+  const step = useSignUpStore((state) => state.step)
+  const errorMessage = useSignUpStore((state) => state.errorMessage)
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => log(data)
+  const onSubmit: SubmitHandler<Inputs> = useCallback(
+    (data) => {
+      const validation = InputsEmailSchema.safeParse(data.email)
+      if (!validation.success) {
+        setErrorMessage('Invalid Email')
+        return
+      }
+      // TODO: go to password setup page
+      increaseStep()
+    },
+    [increaseStep, setErrorMessage]
+  )
   const onClickGoogleLoginButton = useCallback(() => signIn('google'), [])
+
+  useEffect(() => {
+    router.push(`/signup?step=${step}`)
+  }, [router, step])
+
   return (
     <Wrapper>
       <TopTitle>{TITLE_MESSAGE}</TopTitle>
       <EmailForm onSubmit={handleSubmit(onSubmit)}>
-        <TextInput type="email" placeholder="Email" {...register('email')} />
+        <TextInput
+          placeholder="Email"
+          {...register('email', {
+            onChange: (e) => {
+              setErrorMessage('')
+            },
+          })}
+        />
         <EmailNextButton withScale fullWidth>
           {EMAIL_NEXT_MESSAGE}
         </EmailNextButton>
@@ -72,6 +109,7 @@ export default function SignUpForm() {
         onClick={onClickGoogleLoginButton}
         fullWidth
       >{`${LOGIN_PRE_MESSAGE} Google`}</LoginButton>
+      {errorMessage}
     </Wrapper>
   )
 }
