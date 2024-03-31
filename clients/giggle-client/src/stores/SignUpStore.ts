91caa4a6fe65c6d60e6c805ca43d1366313a8b29
yungblud <killi8n@gmail.com>
2024-03-31 12:47:50 +0900

    feat: added errorMessage on signup store

--- clients/giggle-client/src/stores/SignUpStore.ts
+++ clients/giggle-client/src/stores/SignUpStore.ts
@@ -2,30 +2,30 @@ import { create } from 'zustand'
 
 interface SignUpStoreState {
   step: number | null
+  errorMessage: string
 }
 
 interface SignUpStoreAction {
   increaseStep: () => void
   decreaseStep: () => void
+  setErrorMessage: (message: string) => void
 }
 
 type SignUpStore = SignUpStoreState & SignUpStoreAction
 
-const useSignUpStore = () =>
-  create<SignUpStore>((set) => ({
-    step: null,
-    increaseStep: () =>
-      set((state) => ({
-        step: typeof state.step === 'number' ? state.step + 1 : 1,
-      })),
-    decreaseStep: () =>
-      set((state) => ({
-        step: typeof state.step === 'number' ? state.step - 1 : null,
-      })),
-  }))
-
-const SignUpStore = {
-  useSignUpStore,
-}
-
-export default SignUpStore
+export const useSignUpStore = create<SignUpStore>((set) => ({
+  step: null,
+  errorMessage: '',
+  increaseStep: () =>
+    set((state) => ({
+      step: typeof state.step === 'number' ? state.step + 1 : 1,
+    })),
+  decreaseStep: () =>
+    set((state) => ({
+      step: typeof state.step === 'number' ? state.step - 1 : null,
+    })),
+  setErrorMessage: (message) =>
+    set(() => ({
+      errorMessage: message,
+    })),
+}))
