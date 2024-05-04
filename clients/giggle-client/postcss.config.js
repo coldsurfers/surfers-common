cd463551ff96d5581915ccbd4c60780fb5e12712
yungblud <killi8n@gmail.com>
2024-05-04 23:09:37 +0900

    fix: fixed wrong path for postcss

--- clients/giggle-client/postcss.config.js
+++ clients/giggle-client/postcss.config.js
@@ -18,10 +18,7 @@ module.exports = {
     [
       '@fullhuman/postcss-purgecss',
       {
-        content: [
-          './pages/**/*.{js,jsx,ts,tsx}',
-          './components/**/*.{js,jsx,ts,tsx}',
-        ],
+        content: ['./src/**/*.{js,jsx,ts,tsx}'],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
         safelist: ['html', 'body'],
       },
