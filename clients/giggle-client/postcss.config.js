7f32db93a0e06a7b2e7ca05536411122bb369f4e
yungblud <killi8n@gmail.com>
2024-05-04 19:13:48 +0900

    fix: fixed post css

--- clients/giggle-client/postcss.config.js
+++ clients/giggle-client/postcss.config.js
@@ -2,14 +2,29 @@ module.exports = {
   plugins: [
     'tailwindcss',
     'autoprefixer',
-    process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./src/**/*.{js,jsx}'],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : undefined,
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: ['html', 'body'],
+      },
+    ],
   ],
 }
