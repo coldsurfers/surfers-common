66624b3da9abe0c10e9c94cabdc547d5561a58d1
yungblud <killi8n@gmail.com>
2024-05-05 12:57:59 +0900

    feat: modified tailwind.config.js

--- clients/giggle-client/tailwind.config.js
+++ clients/giggle-client/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // purge: [],
   content: [
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
