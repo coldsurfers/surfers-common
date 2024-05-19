7ab644b43e5d99966e0e833a6c48ad4ee7e0e8c3
yungblud <killi8n@gmail.com>
2024-05-19 14:13:41 +0900

    feat: removed billets-admin-client

--- clients/billets-admin-client/utils/storage/types.ts
+++
@@ -1,1 +0,0 @@
-export type StorageItem = '@billets/token'
--- clients/billets-admin-client/utils/validatePassword.ts
+++
@@ -1,5 +0,0 @@
-export default function validatePassword(password: string) {
-  return String(password).match(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/
-  )
-}
