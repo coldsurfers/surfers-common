ab1603ebe3d4a161b28d660b390082da7b23aa03
yungblud <killi8n@gmail.com>
2024-07-12 21:02:40 +0900

    feat: :art: removed wamuseum-client

--- clients/wamuseum-client/src/app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-import { PropsWithChildren } from 'react'
-import { auth } from '@/libs/auth'
-
-export default async function LoginLayout({ children }: PropsWithChildren<{}>) {
-  const session = await auth()
-  if (session) redirect('/')
-  return <>{children}</>
-}
--- clients/wamuseum-client/src/libs/request.ts
+++
@@ -1,8 +0,0 @@
-const host = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:8001' : ''
-
-// eslint-disable-next-line no-undef
-const request = async (path: string, init?: RequestInit) =>
-  // eslint-disable-next-line no-return-await
-  await fetch(`${host}${path}`, init)
-
-export default request
