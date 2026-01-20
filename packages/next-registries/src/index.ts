ecfbbd9ba219d956271c475294f96cb137eb2c1e
yungblud <killi8n@gmail.com>
2026-01-20 10:53:50 +0900

    chore: remove not using packages

--- packages/next-registries/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as StyledComponentsRegistry } from './StyledComponentsRegistry'
-export { default as RegistryProvider } from './RegistryProvider'
--- packages/notion-renderer/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { renderBlock } from './renderBlock'
